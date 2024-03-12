ca4d69b154fa92070507ba532402f2e1e71593f9
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-12 12:15:55 +1030

    Added a number java functions.

---
+++ js_basics/assignment_1/js_functions/number.js
@@ -0,0 +1,11 @@
+console.log(parseInt(3.14));
+console.log(parseInt(0.7));
+console.log(parseInt("32"));
+console.log(parseInt("32asdas"));
+
+
+console.log(parseFloat(3.14));
+console.log(parseFloat(0.7888888888888888888888888888888));
+console.log(parseFloat(0.7999999999999999999999999999999));
+console.log(parseFloat("32"));
+console.log(parseFloat("32asdas"));
