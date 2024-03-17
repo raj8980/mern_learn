6028a0ce2d4c2d73edf1c88acd337d55b0672fc8
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-17 23:50:48 +1030

    Added a new logic for get patient details from hospital.

--- express_class/index.js
+++ express_class/index.js
@@ -4,17 +4,28 @@ const app = express();
 
 app.listen("3000");
 
-app.get("/", (req,res)=>{
-    const a=req.query.n;
-    const ans= sum(a);
-    res.send("Hi There!" + ans);      
-});
+const users = [{
+    name : "Raj",
+    kidneys :[{
+        healthy : false
+    }]
+}];
 
-
-function sum(n){
-    let ans = 0;
-    for(let i = 0; i<=n;i++){
-        ans += i;
+app.get("/", (req,res) => {
+    const patientKidney=users[0].kidneys;
+    const numberOfKidneys = patientKidney.length;
+    let numberOfHealthyKidneys = 0;
+    for(let i = 0; i<numberOfKidneys ; i++){
+        if(patientKidney[i].healthy){
+            numberOfHealthyKidneys = numberOfHealthyKidneys+1;
+        }
     }
-    return ans;
-}
+
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.send({
+        "number  of kidneys " : numberOfKidneys,
+        "number of healthy kidneys" : numberOfHealthyKidneys,
+        "number of unhealthy kidneys" : numberOfUnhealthyKidneys 
+    });
+
+}); 
