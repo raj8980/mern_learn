dcd28bc930e248900f200680769152f232c439cd
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-22 19:51:57 +1030

    Added two calls one for sign in and second for login.

--- express_fetch_auth_mongo/index.js
+++ express_fetch_auth_mongo/index.js
@@ -25,8 +25,36 @@ const allUsers = [
 ];
 
 function userExists(username, password){
-    
+
 }
+
+app.post("/signin",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!userExists(username,password)){
+        return res.status(403).json({
+            msg:"User doesn't exist in our in memory db"
+        });
+    }
+
+    var token = jwt.sign({username:username},"shhhh");
+    return res.json({
+        token
+    });
+});
+
+app.get("/users",function(req,res){
+    const token = req.headers.authorization;
+    try{
+        const decoded = jwt.verify(token, jwtPwd);
+        const username = decoded.username;
+    }catch(err){
+        return res.status(403).json({
+            msg:"Invalid Token"
+        })
+    }
+})
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/views/index.html");
