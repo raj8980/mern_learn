c437d598fb46f6d86ddefc80dbcec937b6a8fda5
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-22 19:46:47 +1030

    Added user data and jwt

--- express_fetch_auth_mongo/index.js
+++ express_fetch_auth_mongo/index.js
@@ -1,8 +1,32 @@
 const express= require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const jwtPwd = "123456";
 
 app.use(express.json());
 app.listen(3000);
+
+const allUsers = [
+    {
+        username : "rajpatel@gmail.com",
+        password : "123",
+        name : "Raj Patel"
+    },
+    {
+        username : "rishipatel@gmail.com",
+        password : "123",
+        name : "Rishi Patel"
+    },
+    {
+        username : "rihanpatel@gmail.com",
+        password : "123",
+        name : "Rihan Patel"
+    }
+];
+
+function userExists(username, password){
+    
+}
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/views/index.html");
