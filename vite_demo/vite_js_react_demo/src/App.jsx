c35c918eb5da37d2c1e6d334906085a6a054e86e
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-25 22:54:44 +1030

    first react example with component and useState hook.

--- vite_demo/vite_js_react_demo/src/App.jsx
+++ vite_demo/vite_js_react_demo/src/App.jsx
@@ -5,13 +5,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-    
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div>    
+        <CustomButton count = {count} setCount = {setCount}></CustomButton>
+        
     </div>
   )
+}
+
+function CustomButton(props){
+  
+  function onClickHandler(){
+    props.setCount(props.count+1);
+  }
+
+  return <button onClick = {onClickHandler}> Counter {props.count}</button>
 }
 
 export default App
