c8cc04b9b8aed6d5adf76671020928cf3d4d154c
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-26 15:46:06 +1030

    Implemented a validation for request body.

--- be_fe_ex_to_do/backend/index.js
+++ be_fe_ex_to_do/backend/index.js
@@ -1,17 +1,40 @@
 const express = require("express");
+const { createTodo,updateTodo } = require("./types");
 
 const app = express();
+
 app.use(express.json());
 
-
 app.get("/todos",(req,res)=>{
+
 
 });
 
 app.post("/add-todo",(req,res)=>{
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+
+    if(!parsedPayload.success){
+        res.status(401).json({
+            msg : "You sent the wrong inputs"
+        });
+        return;
+    } else{
+
+    }
 
 });
 
 app.put("/completed",(req,res)=>{
+    const updatePayload = req.body;
+    const paresedPayload = updateTodo.safeParse(updatePayload);
 
+    if(!parsedPayload.success){
+        res.status(401).json({
+            msg : "You sent the wrong inputs"
+        });
+        return;
+    } else{
+
+    }
 });
