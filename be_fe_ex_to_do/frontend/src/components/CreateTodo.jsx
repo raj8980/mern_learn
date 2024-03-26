7d41b98081fbc249b3361537bb248ee490a4885f
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-26 17:45:39 +1030

    Created a front end in react jsx to add todo and get todo list.

---
+++ be_fe_ex_to_do/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export function CreateTodo() {
+
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+  
+    return (
+    <div>
+      <input style={{ padding: 10,margin: 10 }}
+        onChange = {(e)=>{ 
+             setTitle(e.target.value)
+        // console.log(e.target.value, title);
+                        
+        }}
+        type="text" placeholder="title"></input>
+      <input
+        style={{ padding: 10,margin: 10 }}
+        onChange = {(e)=>{ setDescription(e.target.value) }}
+        type="text" placeholder="description"></input>
+      <button
+        style={{
+          padding: 10,
+          margin: 10,
+        }}
+        onClick={ ()=>{
+             fetch("http://localhost:3000/add-todo",
+            {
+                method:"POST",
+                body : JSON.stringify({
+                    title : title ,
+                    description : description 
+                }),
+                headers:{
+                    "Content-Type" : "application/json"
+                }
+            }).then(async (res) =>{
+                const json = await res.json();
+                alert("Todo added");
+            });
+        }}
+      >Add a todo</button>
+    </div>
+  );
+}
---
+++ be_fe_ex_to_do/frontend/src/components/Todo.jsx
@@ -0,0 +1,16 @@
+export function Todo({todos}){
+    return <div> 
+        {todos.map(todo => {
+        return <div>
+        <h1>
+          {todo.title}
+        </h1>
+        <h2>
+            {todo.description}
+        </h2>
+        <button >{todo.completed == true ? "Completed" : "Mark as completed"}</button>
+    </div>    
+    })}
+    </div>;
+    
+}
