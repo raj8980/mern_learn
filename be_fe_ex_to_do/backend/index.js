d14588cb47df302098a348a291cf403b99dcf542
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-26 16:21:49 +1030

    formatted js file

--- be_fe_ex_to_do/backend/index.js
+++ be_fe_ex_to_do/backend/index.js
@@ -1,58 +1,55 @@
 const express = require("express");
-const { createTodo,updateTodo } = require("./types");
-const {todo} = require("./db");
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/todos",async (req,res)=>{
-    
-    const todos = await todo.find();
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find();
+  res.status(200).json({
+    todos,
+  });
+});
+
+app.post("/add-todo", async (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  } else {
+    await todo.create({
+      title: createPayload.title,
+      description: createPayload.description,
+      completed: false,
+    });
+
     res.status(200).json({
-        todos
-    })
+      msg: "Todo created successfully",
+    });
+  }
 });
 
-app.post("/add-todo",async (req,res)=>{
-    const createPayload = req.body;
-    const parsedPayload = createTodo.safeParse(createPayload);
+app.put("/completed", async (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
 
-    if(!parsedPayload.success){
-        res.status(411).json({
-            msg : "You sent the wrong inputs"
-        });
-        return;
-    } else{
-        await todo.create({
-            title : createPayload.title,
-            description : createPayload.description,
-            completed :  false
-        });
-
-        res.status(200).json({
-            msg : "Todo created successfully"
-        });
-    }
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  } else {
+    await todo.findOneAndUpdate({ _id: updatePayload.id }, { completed: true });
+    res.status(200).json({
+      msg: "Todo marked as completed",
+    });
+  }
 });
 
-app.put("/completed",async (req,res)=>{
-    const updatePayload = req.body;
-    const parsedPayload = updateTodo.safeParse(updatePayload);
-
-    if(!parsedPayload.success){
-        res.status(411).json({
-            msg : "You sent the wrong inputs"
-        });
-        return;
-    } else{
-            await todo.findOneAndUpdate(
-                {_id : updatePayload.id},
-                {completed : true});
-            res.status(200).json({
-                msg : "Todo marked as completed" 
-            })
-    }
-});
-
-app.listen( 3000);
+app.listen(3000);
