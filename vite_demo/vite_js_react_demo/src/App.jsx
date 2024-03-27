7194662362d2f2afe1bf94b68c5b2614d7a65e04
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-27 14:02:45 +1030

    React wrapper implementation

--- vite_demo/vite_js_react_demo/src/App.jsx
+++ vite_demo/vite_js_react_demo/src/App.jsx
@@ -1,53 +1,23 @@
-import React, { Fragment, useState } from "react";
 
-let counter =4;
+
+
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Go to Gym",
-      description: "Go to Gym Today",
-    },
-    {
-      id: 2,
-      title: "eat food",
-      description: "eat food Today",
-    },
-    {
-      id: 3,
-      title: "Learn mern",
-      description: "Learn mern Today",
-    },
-  ]);
 
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: counter++,
-        title: "run",
-        description: "run Today",
-      },
-    ]);
-  }
+  return <div>
+     <CardWrapper innerComponent ={<TextComponent/>}></CardWrapper>
+     </div>
 
-  return (
-    <div>
-      <button onClick={addTodo}>Add todo</button>
-    
-    
-    
-      {todos.map((todo) => 
-        <Todo key={todo.id} title={todo.title} description={todo.description}></Todo>
-      )}
-    </div>
-  );
 }
 
-function Todo({title,description}) {
-  return <div >
-    <h1>{title}</h1>
-    <h5>{description}</h5>
+function TextComponent(){
+  return <div>
+    Hello
+  </div>
+}
+
+function CardWrapper({innerComponent}){
+  return <div  style={{border:"2px solid black"}}>
+      {innerComponent}
   </div>
 }
 
