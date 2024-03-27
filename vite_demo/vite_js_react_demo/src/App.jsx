43afec94ac6810ac5d45f068166cca5e5015b59d
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-03-27 16:54:12 +1030

    Implemented a new ui where it's select the todo out of list and load on page.

--- vite_demo/vite_js_react_demo/src/App.jsx
+++ vite_demo/vite_js_react_demo/src/App.jsx
@@ -3,9 +3,24 @@ import axios from "axios";
 
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [selectedTodo,setSelectedTodo]= useState("66025f8ebf13188543242543");
+  let count =1;
+  let selected_id="66025f8ebf13188543242543";
+
+     useEffect( ()=>{
+        axios.get("http://localhost:3000/todos").then((res)=>{
+          setTodos(res.data.todos)});
+      },[]);
+
   return (
     <div>
-        <Todo id={"66025f8ebf13188543242543"} />
+        {todos.map(todo =>
+        <button value={todo._id} onClick={(e)=>{
+          setSelectedTodo(e.target.value);
+        }}> {todo.title}</button>
+        )}
+        <Todo id={selectedTodo} />
     </div>);
 }
 
@@ -17,9 +32,8 @@ useEffect(()=>{
   .then((res)=>{
       setTodo(res.data.todoData[0]);
   })
+},[id]);
 
-},[]);
-console.log(todo);
 return (<div>
     <h1>
       {todo.title}
