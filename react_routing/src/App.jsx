7a7cb4ed1f67a6bf70989cb0a968d1a360bf8e2f
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-06 13:36:55 +1030

    Implemented default instead of const implementation to add lazy loading

--- react_routing/src/App.jsx
+++ react_routing/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter,Routes, Route, useNavigate } from 'react-router-dom'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+import Dashboard from './components/Dashboard'
+import Landing from  './components/Landing'
 
 function App() {
   
