6ded598f72dd2731faa290099b8c0524afb8ca41
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-06 22:14:32 +1030

    Implemented a prop drilling scenario and fixed by context api.

--- react_routing/src/App.jsx
+++ react_routing/src/App.jsx
@@ -1,43 +1,60 @@
-import './App.css'
-import { BrowserRouter,Routes, Route, useNavigate } from 'react-router-dom'
-import {Suspense, lazy} from 'react';
-const Dashboard = lazy(() => import('./components/Dashboard'))
-const Landing = lazy(() => import('./components/Landing'))
+import "./App.css";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Suspense, lazy } from "react";
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Landing = lazy(() => import("./components/Landing"));
 
 function App() {
-  
+  return (
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense fallback={"Loading ..."}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={"Loading ..."}>
+                <Landing />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Appbar() {
+  const navigate = useNavigate();
 
   return (
     <div>
-      
-      <BrowserRouter>
-      <Appbar/>
-      <Routes>
-          <Route path="/dashboard" element={<Suspense fallback={"Loading ..."}><Dashboard /></Suspense>} />
-          <Route path="/" element={<Suspense fallback={"Loading ..."}><Landing /></Suspense>} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-  )
-}
-
-function Appbar(){
-  const navigate= useNavigate();
-  
-  return (<div>
-    <button onClick={()=>{
-           navigate("/");
-      }}>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         Landing Page
       </button>
-      <button onClick={() =>{
-        navigate("/dashboard");
-      }}>
+      <button
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
         Dashboard Page
       </button>
-      <div style={{background:"black", color: "white"}}>
+      <div style={{ background: "black", color: "white" }}>
         Hi This is the topbar
       </div>
-  </div>);
+    </div>
+  );
 }
-export default App
+export default App;
