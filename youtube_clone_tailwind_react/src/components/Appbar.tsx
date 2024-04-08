f39c0f3b13433c3c52e09700edfaba6a0b76c70f
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-08 12:53:23 +0930

    Implemented a appbar to show case app name searchbar and sign in option at the top of the app.

---
+++ youtube_clone_tailwind_react/src/components/Appbar.tsx
@@ -0,0 +1,13 @@
+export const AppBar = () =>{
+    return <div className="grid grid-cols-3">
+        <div>
+            Youtube
+        </div>
+        <div>
+            Search Bar
+        </div>
+        <div>
+            Sign In
+        </div>
+    </div>
+}
